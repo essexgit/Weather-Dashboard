3bf722312a1340d458c2ffd0287b3b31ba2bfca8
Ralph Cox <73188137+essexgit@users.noreply.github.com>
2023-01-30 19:47:11 +0000

    working model

--- scripts/main.js
+++ scripts/main.js
@@ -1,87 +1,117 @@
 let cityInput = $("#search-form");
 let cityButtonInput = $("#history");
 let cityName = $("#search-input");
-let currentCity = chosenFromLocal();
-let cityChoices = getHistoryFromLocal();
-createCityBtns();
-
 var APIKey = "0aac0c39745eba9e97fdc23093a6dd16";
 
-// from search submit
+createCityBtns();
+cityOnLoad();
+
+
+// form search submit
 cityInput.on("submit", function (event) {
     event.preventDefault();
     let cityCall = cityName.val();
-    let queryForcastURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityCall + "&appid=" + APIKey;
+
+    let queryForecastURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityCall + "&appid=" + APIKey;
+
     $.ajax({
-        url: queryForcastURL,
+        url: queryForecastURL,
         method: 'GET'
     }).then(function (response) {
-        let city = response.city.name;
-        addToSearchHistory(city);
-        chosenToLocal(city);
-        createVariables(response);
-        let newCityBtn = generateBtn(city);
+        let cityForm = response.city.name;
+        addToSearchHistory(cityForm);
+        chosenToLocal(cityForm);
+        let newCityBtn = generateBtn(cityForm);
         cityButtonInput.append(newCityBtn);
+        createVariables(response);
     });
-});
+}
+);
 
 // from button submit
 cityButtonInput.on("click", function (event) {
     event.preventDefault();
-    let city = $(event.target).attr("id");
-    console.log(city);
-    let queryForcastURL = "http://api.openweathermap.org/data/2.5/forecast?q=" +
-        city + "&appid=" + APIKey;
+    let cityGrab = $(event.target).attr("id");
+
+    let queryForecastURL = "http://api.openweathermap.org/data/2.5/forecast?q=" +
+        cityGrab + "&appid=" + APIKey;
     $.ajax({
-        url: queryForcastURL,
+        url: queryForecastURL,
         method: 'GET'
     }).then(function (response) {
-        console.log(response);
+
         chosenToLocal(response.city.name);
         createVariables(response);
     });
 });
 
+function cityOnLoad() {
+    let cityInit = chosenFromLocal();
+
+    let queryForecastURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityInit + "&appid=" + APIKey;
+    $.ajax({
+        url: queryForecastURL,
+        method: 'GET'
+    }).then(function (response) {
+        createVariables(response);
+    });
+}
+
 function createVariables(response) {
     //CurrentCityNow
-    let city = response.city.name;
+    let cityJumbo = response.city.name;
     let rl = response.list;
-    let nowIcon = rl[0].weather[0].icon;
-    let nowDate = rl[0].dt;
-    let nowTemp = rl[0].main.temp;
-    let nowHumidity = rl[0].main.humidity;
+    let nowIconCode = rl[0].weather[0].icon;
+    let nowIconImage = "http://openweathermap.org/img/wn/" + nowIconCode + "@2x.png";
+    let weatherIcon = $(`<img src="${nowIconImage}"/>`);
+    let nowInstant = rl[0].dt;
+    let nowDate = moment.unix(nowInstant).format("DD/MM/yy");
+    let nowTemp = Math.round((rl[0].main.temp - 273.15) * 100) / 100;
     let nowWindSpeed = rl[0].wind.speed;
+    let nowHumidity = rl[0].main.humidity;
+    // print to jumbotron
+    $("#today").text(`${cityJumbo} (${nowDate})`).append(weatherIcon);
+    $("#today-temp").text(`Temp: ${nowTemp} °C`);
+    $("#today-wind").text(`Wind: ${nowWindSpeed} KPH`);
+    $("#today-humidity").text(`Humidity: ${nowHumidity}%`);
     //CurrentCity5Day
     for (let index = 7; index < rl.length; index += 8) {
-        let icon = rl[index].weather[0].icon;
+        let iconCode = rl[index].weather[0].icon;
+        let iconImage = "http://openweathermap.org/img/wn/" + iconCode + ".png";
+        let weatherIcon = $(`<img src="${iconImage}"/>`);
         let instant = rl[index].dt;
-        let date = moment.unix(instant).format("DDD MMMM yyyy");
-        let temp = rl[index].main.temp;
+        let date = moment.unix(instant).format("DD/MM/yy");
+        let temp = Math.round((rl[index].main.temp - 273.15) * 100) / 100;
         let humidity = rl[index].main.humidity;
         let windSpeed = rl[index].wind.speed;
-        console.log(icon);
-        console.log(date);
-        console.log(temp);
-        console.log(humidity);
-        console.log(windSpeed);
+        let posMarker = (index + 1) / 8;
+
+        $(`#Date${posMarker}`).text(date);
+        $(`#Icon${posMarker}`).html("");
+        $(`#Icon${posMarker}`).append(weatherIcon);
+        $(`#Temp${posMarker}`).text(`Temp: ${temp} °C`);
+        $(`#Wind${posMarker}`).text(`Wind: ${windSpeed} KPH`);
+        $(`#Humidity${posMarker}`).text(`Humidity: ${humidity} %`);
     }
 }
 
-function addToSearchHistory(city) {
+function addToSearchHistory(cityAdd) {
     // check list exists, else create
     if (!localStorage.cityHistory) {
         localStorage.setItem('cityHistory', JSON.stringify([]));
     }
     // check city exists, else add
     let cityArray = JSON.parse(localStorage.getItem('cityHistory'));
-    if (!cityArray.includes(city)) {
-        cityArray.push(city);
+    if (!cityArray.includes(cityAdd)) {
+        cityArray.push(cityAdd);
     }
     updateHistoryToLocal(cityArray);
     return;
 }
 
-function createCityBtns(cityArray) {
+function createCityBtns() {
+    // get history
+    let cityChoices = getHistoryFromLocal();
     // loop over Array to get city
     cityChoices.forEach(city => {
         let newCityBtn = generateBtn(city);
@@ -90,15 +120,18 @@ function createCityBtns(cityArray) {
 
 }
 
-function generateBtn(city) {
+function generateBtn(cityBtnNew) {
     let btn = $("<button class=\"cityBtn\">");
-    btn.attr("id", city);
-    btn.append(city);
+    btn.attr("id", cityBtnNew);
+    btn.append(cityBtnNew);
     return btn;
 }
 
 function getHistoryFromLocal() {
-    // get from local storage
+    // get from local storage create if empty
+    if (!localStorage.cityHistory) {
+        localStorage.setItem('cityHistory', JSON.stringify([]));
+    }
     return JSON.parse(localStorage.getItem('cityHistory'));
 }
 
@@ -115,15 +148,9 @@ function chosenToLocal(city) {
 }
 
 function chosenFromLocal() {
-    if (localStorage.lastChoice) { return JSON.parse(localStorage.getItem('lastChoice')); }
-    return;
+    // check last chosen exists or create it
+    if (!localStorage.lastChoice) {
+        localStorage.setItem('lastChoice', JSON.stringify(""));
+    }
+    return JSON.parse(localStorage.getItem('lastChoice'));
 }
-
-function currentWeatherBanner(city) {
-
-}
-
-function futureWeatherCards(city) {
-
-}
-
